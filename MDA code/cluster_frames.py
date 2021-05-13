5c50d196f3ce562b43a18dcd45fd8d49a0a77db1
UranMai <maimekov.uz@gmail.com>
2021-05-13 14:48:25 +0300

    Update clustering
    Description Add selection of Kth and all frames, add weighted centroids

--- MDA code/cluster_frames.py
+++ MDA code/cluster_frames.py
@@ -28,7 +28,8 @@ phi_data = [line for line in phifile if 'ASG' in line]
 
 u = mda.Universe(PDB, XTC)
 prot = u.select_atoms('protein')
-print(f'Number of frames (time steps) - {u.trajectory.n_frames}')
+total_frames = u.trajectory.n_frames # total number of frames 
+print(f'Number of frames (time steps) - {total_frames}')
 
 ###############
 ###Centering###
@@ -51,24 +52,63 @@ u.trajectory.add_transformations(*transforms)
 #            clm.AffinityPropagationNative(),
 #            clm.DBSCAN()]
 
-km1 = clm.KMeans(n_clusters=2, algorithm="auto")
+def cluster_frames(universe, n_clusters, method='k-means'):
+    '''
+    @description
+        Calculate distance (RMSD) matrix between al the frames of the Universe object and cluster by KMeans method based on RMSD matrix. 
+        Find cluster centroid and weights and return their dictionaries
+    @input
+        universe - MDAnalysis Universe object
+        n_clusters - select number of clusters 
+        method - KMeans method and other methods        
+    '''
+    # Define number of clusters 
+    if method == 'k-means':
+        km1 = clm.KMeans(n_clusters=n_clusters, algorithm="auto")
 
-# https://docs.mdanalysis.org/1.0.0/documentation_pages/analysis/encore/confdistmatrix.html
-# calculates the conformational distance (RMSD) matrix. 
-# The distance matrix is calculated between all the frames of all the Universe objects given as input.
-rmsd_matrix = encore.get_distance_matrix(ensemle=u)
+    # https://docs.mdanalysis.org/1.0.0/documentation_pages/analysis/encore/confdistmatrix.html
+    # calculates the conformational distance (RMSD) matrix. 
+    # The distance matrix is calculated between all the frames of all the Universe objects given as input.
+    rmsd_matrix = encore.get_distance_matrix(ensemble=u)
 
-cluster_collection = encore.cluster(ensembles=u, method=km1, distance_matrix=rmsd_matrix)
-centroids = []
-for cluster in cluster_collection.clusters:
-    centroids.append(cluster.centroid)
-    print(f'Cluster {cluster.id} has {cluster.size} elements. Frame {cluster.centroid} is cluster centroid')
+    cluster_collection = encore.cluster(ensembles=u, method=km1, distance_matrix=rmsd_matrix)
+    centroids = {}
+    weights = {}
+    for cluster in cluster_collection.clusters:
+        centroids[cluster.id] = cluster.centroid
+        weights[cluster.id] = cluster.size/total_frames
+        print(f'Cluster {cluster.id} has {cluster.size} elements. Frame {cluster.centroid} is cluster centroid')
+    return centroids, weights
 
-# For selected time steps create dict of atoms positions
+def create_Kth_frames(k, total_frames):
+    k_frames = list(range(total_frames))[::k]
+    return k_frames
+
+
+'''
+For selected time frames create dict of atoms positions
+Write atoms positions for considered frames 
+    'centroid' - select frames from clustering function
+    'all' - select all frames
+    'k_frame' - select each k-th frame from trajectory
+'''
+select = 'k_frame'
 frames_dict = {}
-for ts in u.trajectory:
-    if ts.frame in centroids:
-        frames_dict[ts.frame] = u.atoms.positions
+if select == 'centroid':
+    centroids, weights = cluster_frames(universe=u, n_clusters=2, method='k-means')
+    for ts in u.trajectory:
+        if ts.frame in centroids.values():
+            frames_dict[ts.frame] = u.atoms.positions
+elif select == 'k_frame':
+    k_frames = create_Kth_frames(5, total_frames)
+    for ts in u.trajectory:
+        if ts.frame in k_frames:
+            frames_dict[ts.frame] = u.atoms.positions
+elif select == 'all':
+    frames_dict[ts.frame] = u.atoms.positions
+
+
+print(frames_dict.keys())
 
 prepare_secondary_structure_file(file1, phi_data)
 prepare_rsa_file(file1, phi_data)
@@ -161,8 +201,8 @@ def create_net_files(ts, frames_dict):
     # find_centroid_ligand_bonds(file1, centroid_coords, ligand_centroids)
    
 
-for ts in frames_dict.keys(): # iterate over most valuable time steps
-    create_net_files(ts, frames_dict) # call function to create file with interactions, then run network analysis
+# for ts in frames_dict.keys(): # iterate over most valuable time steps
+    # create_net_files(ts, frames_dict) # call function to create file with interactions, then run network analysis
     # cmd = f"sed -i 's/SYSTEM/-/g' adk_oplsaa_{ts}_net"
     # os.system(cmd)
 
