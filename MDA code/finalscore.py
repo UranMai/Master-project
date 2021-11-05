5516c999b2d694f98b2c3ec9924fcb0b712dfbae
UranMai <54233946+UranMai@users.noreply.github.com>
2021-11-05 15:25:05 +0600

    global update

--- MDA code/finalscore.py
+++ MDA code/finalscore.py
@@ -1,164 +1,116 @@
-# TEST CHANGE
+import os
+import sys
 import pandas as pd
-import sys, re
-import time
-# pdb_file = sys.argv[1]
-t0 = time.time()
-pdb = sys.argv[1]
-
-pdb_file = re.sub(".pdb","",pdb)
-print(pdb_file)
-
-# Read Energetics and Centroid files
-data1 = pd.read_csv(pdb_file+'_scoresEnergetics', sep='\t', index_col='AA')
-data1Z = pd.read_csv(pdb_file + '_scoresEnergeticsZ', sep='\t', index_col='AA')
-data1 = data1.round(6)
-data1Z = data1Z.round(6)
-data1Z = data1Z.fillna(value=0)
-
-data2 = pd.read_csv(pdb_file + '_scoresCentroid', sep='\t', index_col='AA')
-data2Z = pd.read_csv(pdb_file + '_scoresCentroidZ', sep='\t', index_col='AA')
-data2Z = data2Z.round(6)
-
-data2Z.columns = data2Z.columns + 'CENTROIDSC'
-
-data1Multimer = pd.read_csv(pdb_file + '_scoresEnergetics', sep='\t', index_col='AA')
-data1MultimerZ = pd.read_csv(pdb_file + '_scoresEnergeticsZ', sep='\t', index_col='AA')
-data1MultimerZ = data1MultimerZ.round(6)
-data1MultimerZ = data1MultimerZ.fillna(value=0)
-
-data2Multimer = pd.read_csv(pdb_file + '_scoresCentroid', sep='\t', index_col='AA')
-data2MultimerZ =  pd.read_csv(pdb_file + '_scoresCentroidZ', sep='\t', index_col='AA')
-data2MultimerZ = data2MultimerZ.round(6)
-
-# Rename columns
-data1MultimerZ.columns = data1MultimerZ.columns + 'MULTIMER'
-data2MultimerZ.columns = data2MultimerZ.columns + 'MULTIMERCENTROIDSC'
-data2MultimerZ = data2MultimerZ.fillna(value=0)
 
 
-data1 = pd.concat([data2['RSA'], data1],axis=1)
-dataZ = pd.concat([data1Z, data2Z], axis=1)
+def load_monomer_data(pdb_name):
+    """
+    Load monomer data for test structure (1btl.pdb, 1gvp.pdb)
+    """
+    data1 = pd.read_csv(pdb_name + '_scoresEnergetics', sep='\t', index_col='AA')
+    data1Z = pd.read_csv(pdb_name + '_scoresEnergeticsZ', sep='\t', index_col='AA')
+    data1Z = data1Z.fillna(value=0)
 
-acidsMonomer = []
-for i in range(len(dataZ)):
-    node = dataZ.index[i]
-    acidsMonomer.append(node)
+    data2 = pd.read_csv(pdb_name + '_scoresCentroid', sep='\t', index_col='AA')
+    data2Z = pd.read_csv(pdb_name + '_scoresCentroidZ', sep='\t', index_col='AA')
+    data2Z = data2Z.fillna(value=0)
 
-acidsMultimer = []
-for i in range(len(data1MultimerZ)):
-    node = data1MultimerZ.index[i]
-    acidsMultimer.append(node)
+    data2 = data2[data2.index.isin(data1.index)]
+    data2Z = data2Z[data2Z.index.isin(data1Z.index)]
 
-allAcids = list(set(acidsMonomer+acidsMultimer))
+    data2Z.columns = data2Z.columns + 'CENTROIDSC'
 
-
-# Calculate Average value of SecondOrderIntermodularDegree
-SecondOrderIntermodularDegree_ALL = (data1MultimerZ["SecondOrderIntermodularDegreeSTRIDE_sidechainMULTIMER"]+
-                                     data1MultimerZ["SecondOrderIntermodularDegreeSTRIDEMULTIMER"]+
-                                     data2MultimerZ['SecondOrderIntermodularDegreeSTRIDEMULTIMERCENTROIDSC']+
-                                     data2MultimerZ["SecondOrderIntermodularDegreeWALKTRAPMULTIMERCENTROIDSC"])/4
-# print(SecondOrderIntermodularDegree_ALL)
-
-arr = []
-df = SecondOrderIntermodularDegree_ALL
-for i, j in zip(df.index, df.values):
-    acid = i # Before i[;-1] it returned HIS:26:, now it returns HIS:26:A
-    arr.append([acid, j])
-
-new_df = pd.DataFrame(arr)
-# print(new_df)
-SecondOrderIntermodularDegree_AVERAGE = {}
-
-for acid in allAcids:
-    value = new_df[new_df[0]==acid][1].mean()
-    SecondOrderIntermodularDegree_AVERAGE[acid] = value
-
-missing = [acid for acid in allAcids if acid not in acidsMultimer]
-# for missing nodes calculate 
-for node in missing:
-    value = (data1Z[data1Z.index == node]["SecondOrderIntermodularDegreeSTRIDE_sidechain"]+
-             data1Z[data1Z.index == node]["SecondOrderIntermodularDegreeSTRIDE"]+
-             data2Z[data2Z.index == node]["SecondOrderIntermodularDegreeSTRIDECENTROIDSC"]+
-             data2Z[data2Z.index == node]["SecondOrderIntermodularDegreeWALKTRAPCENTROIDSC"])/4
-    SecondOrderIntermodularDegree_AVERAGE[node] = value
-
-# Average value of NodeEdgeBetweenness values
-NodeEdgeBetweennessSTRIDE_AVERAGE_MONOMER = (dataZ["NodeEdgeBetweennessSTRIDE"]+
-                                             dataZ["NodeEdgeBetweennessSTRIDE_sidechain"])/2
-NodeEdgeBetweennessSTRIDE_AVERAGE_MULTIMER = (data1MultimerZ["NodeEdgeBetweennessSTRIDEMULTIMER"]+
-                                              data1MultimerZ["NodeEdgeBetweennessSTRIDE_sidechainMULTIMER"])/2
-NodeEdgeBetweennessSTRIDE_sidechain_ALL = NodeEdgeBetweennessSTRIDE_AVERAGE_MONOMER.append(NodeEdgeBetweennessSTRIDE_AVERAGE_MULTIMER)
-
-arr = []
-df = NodeEdgeBetweennessSTRIDE_sidechain_ALL
-for i, j in zip(df.index, df.values):
-    acid = i # Before i[;-1] it returned HIS:26:, now it returns HIS:26:A
-    arr.append([acid, j])
-new_df = pd.DataFrame(arr)
-
-NodeEdgeBetweennessSTRIDE_sidechain_MAX = {}
-
-for acid in acidsMonomer+acidsMultimer:
-    value = new_df[new_df[0]==acid][1].max()
-    NodeEdgeBetweennessSTRIDE_sidechain_MAX[acid] = value
-
-# print(NodeEdgeBetweennessSTRIDE_sidechain_MAX)
-
-#  CALCULATIONS OF LIGAND VALUES (MIN VALUE)
-LigandMULTIMERCENTROIDSC_ALL = data2MultimerZ["LigandMULTIMERCENTROIDSC"]
-LigandMULTIMERCENTROIDSC_ALL.sum()
-
-arr = []
-df = LigandMULTIMERCENTROIDSC_ALL
-for i, j in zip(df.index, df.values):
-    acid = i # Before i[;-1] it returned HIS:26:, now it returns HIS:26:A
-    arr.append([acid, j])
-new_df = pd.DataFrame(arr)
-
-LigandMULTIMERCENTROIDSC_MIN = {}
-
-for acid in acidsMultimer:
-    value = new_df[new_df[0]==acid][1].min()
-    LigandMULTIMERCENTROIDSC_MIN[acid] = value
-
-# print(LigandMULTIMERCENTROIDSC_MIN)
-
-missing = [acid for acid in allAcids if acid not in acidsMultimer]
-missing
-for node in missing:
-    value = dataZ["LigandCENTROIDSC"]
-    LigandMULTIMERCENTROIDSC_MIN[node] = value
-
-RSA_ALL = pd.concat([data1['RSA'], data2Multimer['RSA']])
-RSA_MIN = {}
-# for node in allAcids:
-#     value = RSA_ALL[RSA_ALL.index.str.contains(node)].min()
-#     RSA_MIN[node] = value
-
-arr = []
-df = RSA_ALL
-for i, j in zip(df.index, df.values):
-    acid = i 
-    arr.append([acid, j])
-new_df = pd.DataFrame(arr)
-
-for acid in acidsMonomer+acidsMultimer:
-    value = new_df[new_df[0]==acid][1].min()
-    RSA_MIN[acid] = value
-
-# Calculations of SCORE
-out = {}
-for node in allAcids:
-    y = (SecondOrderIntermodularDegree_AVERAGE[node]+NodeEdgeBetweennessSTRIDE_sidechain_MAX[node]-LigandMULTIMERCENTROIDSC_MIN[node])
-    out[node] = y
+    return data1, data1Z, data2, data2Z
 
 
-pd.DataFrame(out.items()).sort_values(by=0).to_csv(pdb_file+'_FinalSum', sep='\t', index=False, header=False)
-print('Create FinalSum', time.time()-t0)
+def load_multimer_data(pdb_name):
+    """
+    Load multimer data. For test `2oob` structure consider ubiquitin ligase ligand as HETATM
+    and run scripts for it with (multimer data) and without (monomer data) ligase ligand
+    dir_path - points out the path to folder with multimer data
+    for monomer data it's same folder as for function `load_monomer_data`
+    """
+    dir_path = '.'
+    pdb_name = os.path.join(dir_path, pdb_name)
+    data1Multimer = pd.read_csv(pdb_name + '_scoresEnergetics', sep='\t', index_col='AA')
+    data1MultimerZ = pd.read_csv(pdb_name + '_scoresEnergeticsZ', sep='\t', index_col='AA')
+    data1MultimerZ = data1MultimerZ.fillna(value=0)
 
-# Check results with R.sum(y) (use round())
-sum = 0 
-for i, j in out.items():
-   sum += round(j, 1) # change 1to2,3
-print(sum)
+    data2Multimer = pd.read_csv(pdb_name + '_scoresCentroid', sep='\t', index_col='AA')
+    data2MultimerZ = pd.read_csv(pdb_name + '_scoresCentroidZ', sep='\t', index_col='AA')
+    data2MultimerZ = data2MultimerZ.fillna(value=0)
+
+    data2Multimer = data2Multimer[data2Multimer.index.isin(data1Multimer.index)]
+    data2MultimerZ = data2MultimerZ[data2MultimerZ.index.isin(data1MultimerZ.index)]
+
+    data1MultimerZ.columns = data1MultimerZ.columns + 'MULTIMER'
+    data2MultimerZ.columns = data2MultimerZ.columns + 'MULTIMERCENTROIDSC'
+
+    return data1Multimer, data1MultimerZ, data2Multimer, data2MultimerZ
+
+
+if __name__ == "__main__":
+    """
+    Final Score of amino acid is result of :
+    1. Second Order Intermodular Degree - two degrees of separation between residues from different higher-order structures, 
+    as an average of classical secondary structure and Walktrap definitions.
+    2. Node-Edge Betweenness - the summed frequency that a node’s edges were utilized as a shortest path between
+    all pairs of nodes in the network
+    3. Euclidean Distance from Centroid to Ligand - the distance b/w residue’s centroid and the center of mass of the protein’s ligand.
+    If multimer exists, 1. and 3. calculated for only multimer conformation, 2. for monomer and multimer
+    The path to multimer is considered in function `load_multimer_data`
+    """
+    pdb = sys.argv[1]
+    pdb_name = os.path.splitext(pdb)[0]
+
+    data1, data1Z, data2, data2Z = load_monomer_data(pdb_name)
+    data1Multimer, data1MultimerZ, data2Multimer, data2MultimerZ = load_multimer_data(pdb_name)
+
+    data1 = pd.merge(data2[['RSA']], data1, on='AA')
+    dataZ = pd.merge(data1Z, data2Z, on='AA')
+
+    # monomer acids and multimer acid can differ, so consider missing nodes (monomeric)
+    acidsMonomer = dataZ.index.to_list()
+    acidsMultimer = data1MultimerZ.index.to_list()
+    allAcids = list(set(acidsMonomer + acidsMultimer))
+    missingNodes = [acid for acid in allAcids if acid not in acidsMultimer]
+
+    # Calculate SecondOrderIntermodularDegree as the average of the multimer only
+    SecondOrderIntermodularDegree_ALL = (data1MultimerZ["SecondOrderIntermodularDegreeSTRIDE_sidechainMULTIMER"] +
+                                         data1MultimerZ["SecondOrderIntermodularDegreeSTRIDEMULTIMER"] +
+                                         data2MultimerZ['SecondOrderIntermodularDegreeSTRIDEMULTIMERCENTROIDSC'] +
+                                         data2MultimerZ["SecondOrderIntermodularDegreeWALKTRAPMULTIMERCENTROIDSC"]) / 4
+
+    SecondOrderIntermodularDegree_AVERAGE = SecondOrderIntermodularDegree_ALL.to_dict()
+    for node in missingNodes:
+        value = (data1Z[data1Z.index == node]["SecondOrderIntermodularDegreeSTRIDE_sidechain"] +
+                 data1Z[data1Z.index == node]["SecondOrderIntermodularDegreeSTRIDE"] +
+                 data2Z[data2Z.index == node]["SecondOrderIntermodularDegreeSTRIDECENTROIDSC"] +
+                 data2Z[data2Z.index == node]["SecondOrderIntermodularDegreeWALKTRAPCENTROIDSC"]) / 4
+        SecondOrderIntermodularDegree_AVERAGE[node] = value
+
+    # NodeEdgeBetweenness is the max of the monomer or multimer
+    NodeEdgeBetweennessSTRIDE_AVERAGE_MONOMER = (dataZ["NodeEdgeBetweennessSTRIDE"] + dataZ["NodeEdgeBetweennessSTRIDE_sidechain"]) / 2
+    NodeEdgeBetweennessSTRIDE_AVERAGE_MULTIMER = (data1MultimerZ["NodeEdgeBetweennessSTRIDEMULTIMER"] +
+                                                  data1MultimerZ["NodeEdgeBetweennessSTRIDE_sidechainMULTIMER"]) / 2
+
+    NodeEdgeBetweennessSTRIDE_sidechain_ALL = NodeEdgeBetweennessSTRIDE_AVERAGE_MONOMER.append(NodeEdgeBetweennessSTRIDE_AVERAGE_MULTIMER)
+    df = NodeEdgeBetweennessSTRIDE_sidechain_ALL
+    NodeEdgeBetweennessSTRIDE_sidechain_MAX = {acid: df[df.index == acid].max() for acid in allAcids}
+
+    # Ligand is the min of the multimer only
+    LigandMULTIMERCENTROIDSC_ALL = data2MultimerZ["LigandMULTIMERCENTROIDSC"]
+    df = LigandMULTIMERCENTROIDSC_ALL
+    LigandMULTIMERCENTROIDSC_MIN = {acid: df[df.index == acid].min() for acid in acidsMultimer}
+
+    for node in missingNodes:
+        value = dataZ['LigandCENTROIDSC']
+        LigandMULTIMERCENTROIDSC_MIN[node] = value
+
+    # Calculations of FINAL SCORE
+    final_score = {}
+    for node in allAcids:
+        score = (SecondOrderIntermodularDegree_AVERAGE[node] + NodeEdgeBetweennessSTRIDE_sidechain_MAX[node] - LigandMULTIMERCENTROIDSC_MIN[node])
+        final_score[node] = score
+
+    pd.DataFrame(final_score.items()).sort_values(by=0).to_csv(pdb_name + '_FinalSum', sep='\t', index=False, header=False)
